c2e800bbd7c3cf2b0b2db7212ab37c09dfaf5cb0
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2022-10-16 02:02:30 +0800

    Delete modul 2 directory

--- modul 2/soal 04/PRAK104-2210817220034-Wanda Karliyanti.py
+++
@@ -1,8 +0,0 @@
-a = 400000
-b = 350000
-x = a-(a*0.13)
-y = b-(b*0.21)
-print("Harga sepatu A adalah ", a)
-print("Harga sepatu B adalah ", b)
-print("Sepatu A mendapat diskon 13% sehingga harganya menjadi = ", round(x))
-print("Sepatu A mendapat diskon 21% sehingga harganya menjadi = ", round(y))
