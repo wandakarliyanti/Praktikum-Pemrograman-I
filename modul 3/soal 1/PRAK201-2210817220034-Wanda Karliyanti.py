0521c066d88ea7afd7069ecc30e470d657b38385
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2022-10-26 19:54:18 +0800

    Delete modul 3/soal 1 directory

--- modul 3/soal 1/PRAK201-2210817220034-Wanda Karliyanti.py
+++
@@ -1,7 +0,0 @@
-input("Nama                     : ")
-input("NIM                      : ")
-input("Kelas Paralel            : ")
-input("Tempat/Tanggal Lahir     : ")
-input("Alamat                   : ")
-input("Hobby                    : ")
-input("No. Hp                   : ")
