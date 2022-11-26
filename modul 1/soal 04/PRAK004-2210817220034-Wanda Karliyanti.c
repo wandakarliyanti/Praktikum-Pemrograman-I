28fd4c54de92d2eb4e2744dfc8052cd88eec71fe
Wanda Karliyanti <115330174+wandakarliyanti@users.noreply.github.com>
2022-11-27 01:18:41 +0800

    Add files via upload

---
+++ modul 1/soal 04/PRAK004-2210817220034-Wanda Karliyanti.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+printf("##############################\n");
+printf("#                            #\n");
+printf("#      Wanda Karliyanti      #\n");
+printf("#        2210817220034       #\n");
+printf("#                            #\n");
+printf("##############################");
+
+}
